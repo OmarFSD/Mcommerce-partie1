660f482a8f350d87f3ae2ba88d58351fce79da7d
oboukhobza <omar.boukhobza@digimind.com>
2019-08-06 11:24:30 +0100

    [Projet MicroCommerce] Partie 2 - Tri par ordre alphabétique

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -117,4 +118,9 @@ public class ProductController {
 
 
 
+    @GetMapping(value = "/triProduits")
+    public List<Product> trierProduitsParOrdreAlphabetique () {
+        return productDao.findAll(new Sort("nom"));
+    }
+    
 }
